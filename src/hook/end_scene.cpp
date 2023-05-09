e1ff760551631c34b75e7824db74e3f903cd92b2
Olafur Waage <olafurw@gmail.com>
2023-05-10 01:19:36 +0200

    notes and determinism check

--- src/hook/end_scene.cpp
+++ src/hook/end_scene.cpp
@@ -5,22 +5,51 @@
 #include "imgui_impl_dx9.h"
 
 #include <chrono>
+#include <cmath>
+#include <vector>
+#include <format>
+
+std::vector<std::string> lagFrames;
+
+void PressButton(WORD button) {
+  INPUT ip;
+  ip.type = INPUT_KEYBOARD;
+  ip.ki.time = 0;
+  ip.ki.wScan = button; // space
+  ip.ki.dwFlags = KEYEVENTF_SCANCODE;
+  SendInput(1, &ip, sizeof(INPUT));
+}
+
+void ReleaseButton(WORD button) {
+  INPUT ip;
+  ip.type = INPUT_KEYBOARD;
+  ip.ki.time = 0;
+  ip.ki.wScan = button;
+  ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+  SendInput(1, &ip, sizeof(INPUT));
+}
 
 EndScene_t D3DEndScene_orig;
 
 static unsigned int frameCounter{ 0 };
 static auto frame_time{ std::chrono::high_resolution_clock::now() };
-static std::chrono::milliseconds delta{ std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - frame_time) };
+static std::chrono::microseconds delta{ std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - frame_time) };
+static constexpr long long frame_ms{ 33333 };
+static long long frame_lag{ 0 };
 
 bool imguiInit{ false };
 bool showMenu{ true };
+constexpr uint16_t btn_right{ 0x20 };
+constexpr uint16_t btn_left{ 0x1e };
+constexpr uint16_t btn_space{ 0x39 };
+
 struct CurrentProcess {
   HMODULE hModule;
   HWND TargetHwnd;
   WNDPROC TargetWndProc;
 } Process;
 
-extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
   if (showMenu && ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam)) {
     return true;
@@ -57,12 +86,25 @@ HRESULT WINAPI D3DEndSceneHook(IDirect3DDevice9 * device) {
   ImGui::NewFrame();
 
   bool open{ true };
-  ImGui::SetNextWindowSize(ImVec2(120, 70));
+  ImGui::SetNextWindowSize(ImVec2(120, 200));
   ImGui::SetNextWindowPos(ImVec2(10, 10));
   ImGui::Begin("Debug", &open);
 
   ImGui::Text("frame: %d", frameCounter);
-  ImGui::Text("ms: %d", delta.count());
+
+  const double d{ delta.count() / 1000.0f };
+  ImGui::Text("ms: %.3f", d);
+
+  const double fl{ frame_lag / 1000.0f };
+  ImGui::Text("drift: %.3f", fl);
+
+  std::vector<const char *> lagFramesC;
+  for (const auto & s : lagFrames) {
+    lagFramesC.push_back(s.c_str());
+  }
+  int currentLagFrame = 0;
+
+  ImGui::ListBox("lag frames", &currentLagFrame, lagFramesC.data(), lagFramesC.size());
 
   ImGui::End();
   ImGui::EndFrame();
@@ -72,166 +114,57 @@ HRESULT WINAPI D3DEndSceneHook(IDirect3DDevice9 * device) {
   const HRESULT result{ D3DEndScene_orig(device) };
 
   if (frameCounter == 1) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39; // space
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-  if (frameCounter == 2) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-  if (frameCounter == 3) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-  if (frameCounter == 4) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-
-  // uh oh...
-  if (frameCounter == 148) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-  if (frameCounter == 149) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-
-  // is everything ok?
-  if (frameCounter == 150) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-  if (frameCounter == 151) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-
-  // no we hit some sort of interferance
-  if (frameCounter == 154) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-  if (frameCounter == 155) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-
-  // going to crash
-  if (frameCounter == 189) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-  if (frameCounter == 190) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-
-  // that was scary
-  if (frameCounter == 460) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-  if (frameCounter == 461) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-
-  // at least we all escaped
-  if (frameCounter == 462) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-  if (frameCounter == 463) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-
-  // guys?
-  if (frameCounter == 560) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE;
-    SendInput(1, &ip, sizeof(INPUT));
-  }
-  if (frameCounter == 561) {
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.time = 0;
-    ip.ki.wScan = 0x39;
-    ip.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
+    PressButton(btn_space);
+  } else if (frameCounter == 2) {
+    ReleaseButton(btn_space);
+  } else if (frameCounter == 3) {
+    PressButton(btn_space);
+  } else if (frameCounter == 4) {
+    ReleaseButton(btn_space);
+  } else if (frameCounter == 5) {
+    PressButton(btn_space);
+  } else if (frameCounter == 6) {
+    ReleaseButton(btn_space);
+  } else if (frameCounter > 50 && frameCounter < 58) {
+    PressButton(btn_right);
+  } else if (frameCounter == 58) {
+    ReleaseButton(btn_right);
+  } else if (frameCounter > 70 && frameCounter < 87) {
+    PressButton(btn_left);
+  } else if (frameCounter == 87) {
+    ReleaseButton(btn_left);
+  } else if (frameCounter > 100 && frameCounter < 118) {
+    PressButton(btn_right);
+  } else if (frameCounter == 118) {
+    ReleaseButton(btn_right);
+  } else if (frameCounter > 130 && frameCounter < 147) {
+    PressButton(btn_left);
+  } else if (frameCounter == 147) {
+    ReleaseButton(btn_left);
+  } else if (frameCounter > 160 && frameCounter < 178) {
+    PressButton(btn_right);
+  } else if (frameCounter == 178) {
+    ReleaseButton(btn_right);
+  } else if (frameCounter > 190 && frameCounter < 207) {
+    PressButton(btn_left);
+  } else if (frameCounter == 207) {
+    ReleaseButton(btn_left);
+  } else if (frameCounter > 220 && frameCounter < 238) {
+    PressButton(btn_right);
+  } else if (frameCounter == 238) {
+    ReleaseButton(btn_right);
+  } else if (frameCounter > 250 && frameCounter < 267) {
+    PressButton(btn_left);
+  } else if (frameCounter == 267) {
+    ReleaseButton(btn_left);
   }
 
   const auto time_now{ std::chrono::high_resolution_clock::now() };
-  delta = std::chrono::duration_cast<std::chrono::milliseconds>(time_now - frame_time);
+  delta = std::chrono::duration_cast<std::chrono::microseconds>(time_now - frame_time);
+  frame_lag = delta.count() - frame_ms;
+  if (frame_lag > 3000) {
+    lagFrames.push_back(std::to_string(frame_lag));
+  }
   frame_time = time_now;
 
   return result;
