940fb9ac57bb9058ede221bbae82d444dd275da7
Olafur Waage <olafurw@gmail.com>
2023-09-17 00:01:02 +0200

    Fixing 32bit build

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -16,3 +16,4 @@ endif()
 
 set(CMAKE_TOOLCHAIN_FILE "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake")
+set(VCPKG_TARGET_TRIPLET "x86-windows" CACHE STRING "VCPKG Target Triplet to use")
